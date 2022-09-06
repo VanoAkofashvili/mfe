bb152f29ad868d54d7cd89428011d91fd4592962
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2022-09-06 11:17:20 +0400

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createMemoryHistory, createBrowserHistory } from "history";
-
 import App from "./App";
 
 // Mount function to start up the app
-function mount(el, { onNavigate, defaultHistory, initialPath }) {
+function mount(el, { onNavigate, defaultHistory, initialPath, onSignIn }) {
   const history =
     defaultHistory ||
     createMemoryHistory({
@@ -16,10 +15,11 @@ function mount(el, { onNavigate, defaultHistory, initialPath }) {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
+      console.log("onParentNavigate");
       const { pathname } = history.location;
       if (pathname !== nextPathname) {
         history.push(nextPathname);
@@ -30,7 +30,7 @@ function mount(el, { onNavigate, defaultHistory, initialPath }) {
 
 // Development in isolation
 if (process.env.NODE_ENV === "development") {
-  const el = document.getElementById("_marketing-dev-root");
+  const el = document.getElementById("_auth-dev-root");
 
   if (el) {
     mount(el, { defaultHistory: createBrowserHistory() });
